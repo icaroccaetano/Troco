b19b0adeae2a0e8d0667b977a22c0903fa9f7062
icaroccaetano <icaroccaetano@gmail.com>
2024-03-29 22:23:11 -0300

    Atualizacoes nos testes e nos metodos

--- Troco.Dominio/TrocoStar.cs
+++ Troco.Dominio/TrocoStar.cs
@@ -2,9 +2,119 @@
 {
     public class TrocoStar
     {
-        public decimal ValorTroco (decimal valorCompra, decimal valorPago)
+        public decimal CalcularTroco (decimal valorCompra, decimal valorPago)
         {
             return valorPago - valorCompra;
         }
-    }
+
+        /*
+         As cedulas do troco sao dadas via lista
+         O numero de cada cedula e dado em ordem decrescente
+         { qnt_notas_200, qnt_notas_100, qnt_notas_50, qnt_notas_20, qnt_notas_10, qnt_notas_5, qnt_notas_2, qnt_moedas_1, qnt_moedas_50, qnt_moedas_25, qnt_moedas_10, qnt_moedas_5 }    
+        */
+
+        public List<int> CalcularCedulas(decimal valorTroco)
+        {
+            int contadorQuantidade = 0;
+            List<int> cedulas = new List<int>();
+
+            while (valorTroco >= 200)
+            {
+                contadorQuantidade++;
+                valorTroco -= 200;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 100)
+            {
+                contadorQuantidade++;
+                valorTroco -= 100;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 50)
+            {
+                contadorQuantidade++;
+                valorTroco -= 50;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+            
+            while (valorTroco >= 20)
+            {
+                contadorQuantidade++;
+                valorTroco -= 20;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 10)
+            {
+                contadorQuantidade++;
+                valorTroco -= 10;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 5)
+            {
+                contadorQuantidade++;
+                valorTroco -= 5;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 2)
+            {
+                contadorQuantidade++;
+                valorTroco -= 2;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 1)
+            {
+                contadorQuantidade++;
+                valorTroco -= 1;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 0.5m)
+            {
+                contadorQuantidade++;
+                valorTroco -= 0.5m;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 0.25m)
+            {
+                contadorQuantidade++;
+                valorTroco -= 0.25m;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 0.10m)
+            {
+                contadorQuantidade++;
+                valorTroco -= 0.10m;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            while (valorTroco >= 0.05m)
+            {
+                contadorQuantidade++;
+                valorTroco -= 0.05m;
+            }
+            cedulas.Add(contadorQuantidade);
+            contadorQuantidade = 0;
+
+            return cedulas;
+        }
+    }  
 }
--- Troco.Testes/UnitTest1.cs
+++ Troco.Testes/UnitTest1.cs
@@ -12,11 +12,12 @@ namespace Troco.Testes
             var troco = new TrocoStar();
 
             //Action
-            var resultado = troco.ValorTroco(120, 150);
+            var resultado = troco.CalcularTroco(120, 150);
 
             //Assert
             Assert.Equal(30, resultado);
         }
+        
         [Fact]
         public void Ao_comprar_50_centavos_e_pagar_1_o_troco_deve_ser_50_centavos()
         {
@@ -24,10 +25,49 @@ namespace Troco.Testes
             var troco = new TrocoStar();
 
             //Action
-            var resultado = troco.ValorTroco(0.50m, 1);
+            var resultado = troco.CalcularTroco(0.50m, 1);
 
             //Assert
             Assert.Equal(0.50m, resultado);
+        }
+        
+        [Fact]
+        public void Ao_dar_100_reais_o_troco_deve_ser_1_cedula_de_100()
+        {
+            //Arrange
+            var troco = new TrocoStar();
+
+            //Action
+            var resultado = troco.CalcularCedulas(100);
+
+            //Assert
+            Assert.Equal(new List<int> { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, resultado);
+        }
+
+        [Fact]
+        public void Ao_dar_387_reais_o_troco_deve_ser_1_cedula_de_200_1_de_100_1_de_50_1_de_20_1_de_10_1_de_5_e_1_de_2()
+        {
+            //Arrange
+            var troco = new TrocoStar();
+
+            //Action
+            var resultado = troco.CalcularCedulas(387);
+
+            //Assert
+            Assert.Equal(new List<int> { 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 }, resultado);
+        }
+
+        [Fact]
+        public void Ao_dar_133_e_75_centavos_o_troco_deve_ser_1_cedula_de_100_1_de_20_1_de_10_1_de_2_1_de_1_1_1_de_50centavos_e_1_25centavos ()
+        {
+            //Arrange
+            var troco = new TrocoStar();
+
+            //Action
+            var resultado = troco.CalcularCedulas(133.75m);
+
+            //Assert
+            Assert.Equal(new List<int> { 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0 }, resultado);
         }
     }
 }
